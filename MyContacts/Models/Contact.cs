490729deaa3860da40926f322715dfe584eb8e77
Isaac Peña <isaacpm01@outlook.com>
2024-08-15 18:33:18 -0400

    Add project files.

---
+++ MyContacts/Models/Contact.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyContacts.Models
+{
+    public class Contact
+    {
+        //PK for the Contact Table
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter a last name")]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter a phone number")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter an email")]
+        public string Email { get; set; } = null!;
+
+        //FK for the Category Table
+
+        [Required(ErrorMessage = "Please select a category")]
+        public int? CategoryId { get; set; }
+
+        [ValidateNever]
+        public Category Category { get; set; } = null!;
+
+        public DateTime DateAdded { get; set; }
+
+        public string Slug => FirstName?.Replace(' ', '-').ToLower() + '-' + LastName?.Replace(" ", "-").ToLower();
+    }
+}
---
+++ MyContacts/Models/ContactContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyContacts.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Contact> Contacts { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Family" },
+                new Category { CategoryId = 2, Name = "Friend" },
+                new Category { CategoryId = 3, Name = "Work" });
+
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact 
+                { 
+                    ContactId = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    PhoneNumber = "123-456-7890",
+                    Email = "john.doe@gmail.com",
+                    CategoryId = 1},
+
+                new Contact
+                {
+                    ContactId = 2,
+                    FirstName = "Bob",
+                    LastName = "Barth",
+                    PhoneNumber = "123-456-7890",
+                    Email = "bobbarth@gmail.com",
+                    CategoryId = 1
+                },
+
+                new Contact
+                {
+                    ContactId = 3,
+                    FirstName = "Cathy",
+                    LastName = "Spencer",
+                    PhoneNumber = "123-456-7890",
+                    Email = "kspencer@gmail.com",
+                    CategoryId = 1
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
